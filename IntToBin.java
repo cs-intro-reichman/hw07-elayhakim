62869874052dc100f02c832f894ef19519bef460
elayhakim <elay.hakim@post.runi.ac.il>
2025-12-26 16:10:03 +0200

    7

--- IntToBin.java
+++ IntToBin.java
@@ -8,7 +8,13 @@ public class IntToBin {
 
     /** Returns the binary representation of the given integer, as a string of 0's and 1's.  */
     public static String toBinary(int x) {
-        //// Replace the following statement with your code
-        return "";
+        int y =x;
+        if (y==1) {
+            return "1";
+        }        
+        if (y==0) {
+            return "0";
+        }    
+        return toBinary(y/2)+ (y%2);
     }    
  }
--- Koch.java
+++ Koch.java
@@ -28,7 +28,20 @@ public class Koch {
      *  and draws a Koch curve of depth n from (x1,y1) to (x2,y2). */
 	public static void curve(int n, double x1, double y1, double x2, double y2) {
 		StdDraw.line(x1, y1, x2, y2);
-		//// Write the rest of your code below.
+		if (n == 0) {
+            StdDraw.line(x1, y1, x2, y2);
+        } else {
+            double xx1 = x1 + (x2 - x1) * (1.0 / 3);
+            double yy1 = y1 + (y2 - y1) * (1.0 / 3);  
+            double xx2 = x1 + (x2 - x1) * 2.0 * (1.0 / 3);
+            double yy2 = y1 + (y2 - y1) * 2.0 * (1.0 / 3);
+            double xtip = (Math.sqrt(3) / 6) * (yy1 - yy2) + 0.5 * (xx1 + xx2);
+            double ytip = (Math.sqrt(3) / 6) * (xx2 - xx1) + 0.5 * (yy1 + yy2);
+            curve(n - 1, x1, y1, xx1, yy1);
+            curve(n - 1, xx1, yy1, xtip, ytip);
+            curve(n - 1, xtip, ytip, xx2, yy2);
+            curve(n - 1, xx2, yy2, x2, y2);
+        }
 	}
 
     /** Gets n, and draws a Koch snowflake of n edges in the standard canvass. */
@@ -36,7 +49,11 @@ public class Koch {
 		// A little tweak that makes the drawing look better
 		StdDraw.setYscale(0, 1.1);
 		StdDraw.setXscale(0, 1.1);
-		// Draws a Koch snowflake of depth n
-		//// Write the rest of your code below.
+		double x1 = 0.2, y1 = 0.65;
+        double x2 = 0.8, y2 = 0.65;
+        double x3 = 0.5, y3 = 0.65 - (Math.sqrt(3) / 2) * 0.6;
+        curve(n, x1, y1, x2, y2);
+        curve(n, x2, y2, x3, y3);
+        curve(n, x3, y3, x1, y1);
 	}
 }
